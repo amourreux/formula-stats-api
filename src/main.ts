78255449a101e41414a100baa8e4187f391f7b8a
Emre Portakal <emre.portakal@xenonsmart.com>
2024-02-14 15:49:16 +0300

    added swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Formula Case Study')
+    .setDescription('Formula Case Study API for reviewing formula 1 stats.')
+    .setVersion('1.0')
+    .addTag('f1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
