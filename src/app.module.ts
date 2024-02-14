6d22dd363b38d1a66789e8ff610fe5213d970b88
Emre Portakal <emre.portakal@xenonsmart.com>
2024-02-14 16:10:50 +0300

    added drivers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DriversModule } from './drivers/drivers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DriversModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
